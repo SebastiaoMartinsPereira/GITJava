3f6a40d22d71f330461e609fc48af3122d7b5ee0
Sebastião Martins Pereira <thiagoedanielamartins@gmail.com>
2015-08-23 18:38:33 -0300

    JavaWeb

---
+++ JAVA EE/fj21-agenda/src/br/com/npsoftwares/jdbc/modelo/Funcionario.java
@@ -0,0 +1,38 @@
+package br.com.npsoftwares.jdbc.modelo;
+
+public class Funcionario {
+
+	Long id;
+	String nome;
+	String usuario;
+	String senha;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
+}
